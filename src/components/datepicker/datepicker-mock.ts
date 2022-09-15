d003d497b1760632d355cdc857f873155cff26cf
Txai Mostardeiro <redtxai@gmail.com>
2022-09-15 23:10:11 +0200

    Comment days array

--- src/components/datepicker/datepicker-mock.ts
+++ src/components/datepicker/datepicker-mock.ts
@@ -1,6 +1,6 @@
 import { OptionType } from "../Select/Select";
 
-const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+// const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"]
 
 export const generateMockDateTime = () => {
