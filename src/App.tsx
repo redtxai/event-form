343d4404294c2bff8cd8bea612bc41c3bc19b23f
Txai Mostardeiro <redtxai@gmail.com>
2022-09-15 10:33:28 +0200

    Setup additional packages and setup the global file for styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
+      <label htmlFor="first-name" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">First name</label>
+      <div className="mt-1 sm:col-span-2 sm:mt-0">
+        <input type="text" name="first-name" id="first-name" autoComplete="given-name" className="block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm" />
+      </div>
+    </div>
   )
 }
 
