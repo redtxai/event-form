083e561fb9befcb764211585ae23153b82668817
Txai Mostardeiro <redtxai@gmail.com>
2022-09-15 11:18:54 +0200

    #1 Test link with issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import './App.css';
+import Label from './components/label/Label';
 
 function App() {
   return (
     <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
-      <label htmlFor="first-name" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">First name</label>
-      <div className="mt-1 sm:col-span-2 sm:mt-0">
+      <Label htmlFor='first-name' text='First Name'></Label>
+      <div className="mt-1 sm:col-span-2 sm:mt-0 blue regal-blue">
+        test
         <input type="text" name="first-name" id="first-name" autoComplete="given-name" className="block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm" />
       </div>
     </div>
