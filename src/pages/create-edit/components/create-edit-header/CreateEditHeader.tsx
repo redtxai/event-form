8db1f0d41af3542fd339a888f5ef07ec031a4d31
Txai Mostardeiro <redtxai@gmail.com>
2022-09-19 09:13:11 +0200

    Adjustments and add the close on the top-right corner

--- src/pages/create-edit/components/create-edit-header/CreateEditHeader.tsx
+++ src/pages/create-edit/components/create-edit-header/CreateEditHeader.tsx
@@ -4,15 +4,15 @@ import Title from "../../../../components/title/Title"
 
 const CreateEditHeader = memo(() => {
   return <header>
-      <p className="relative w-full sm:h-16"></p>
+      <p className="relative w-full sm:h-16"><img className="absolute top-2 right-3 sm:top-7 sm:right-7 cursor-pointer" src="/assets/img/close.svg" alt="close"/></p>
       <section className="max-w-xl m-auto bg-white px-3 py-4 sm:px-6">
-        <div className="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap">
-        <div className="ml-4 mt-2">
-          <Title type="large" text="Event form"></Title>
-        </div>
-        <div className="ml-4 mt-2 flex-shrink-0">
-          <Button className="text-sm leading-4-05 py-2 px-8 rounded-md text-slate-650 bg-white border border-gray-150" text="Edit"></Button>
-        </div>
+        <div className="mt-4 flex flex-wrap items-center justify-between sm:mt-0 sm:mb-4 sm:flex-nowrap">
+          <div>
+            <Title type="large" text="Event form"></Title>
+          </div>
+          <div className="flex-shrink-0">
+            <Button className="text-sm leading-4-05 py-2 px-8 rounded-md text-slate-650 bg-white border border-gray-150" text="Edit"></Button>
+          </div>
       </div>
     </section>
   </header>
